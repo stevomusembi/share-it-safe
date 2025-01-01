6851e9dbaf1451a9b9efc1eb32fa5fe2e853534c
stevenmusembi <musembisteven@gmail.com>
2025-01-01 20:10:57 +0300

    testing why the bucket name is undefined

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -24,6 +24,8 @@ export async function POST(request: Request) {
         const buffer = Buffer.from(await file.arrayBuffer());
         const fileName = `${Date.now()}-${file.name}`;
 
+        console.log("THE BUCKET NAME IS ==>", process.env.BUCKET_NAME);
+
         // Upload the file
         const uploadCommand = new PutObjectCommand({
             Bucket: process.env.BUCKET_NAME,
